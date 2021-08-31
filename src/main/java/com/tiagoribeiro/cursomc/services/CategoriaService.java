0944420b58f1358114da54e3859ea1f964d3648d
Tiago Ribeiro <tgwebmaster27@gmail.com>
2021-08-31 18:09:35 -0300

    validacao sintatica com Bean validation

--- src/main/java/com/tiagoribeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiagoribeiro/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiagoribeiro.cursomc.domain.Categoria;
+import com.tiagoribeiro.cursomc.dto.CategoriaDTO;
 import com.tiagoribeiro.cursomc.repositories.CategoriaRepository;
 import com.tiagoribeiro.cursomc.services.exceptions.DataIntegrityException;
 import com.tiagoribeiro.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome()) ;
 	}
 }
